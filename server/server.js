7cc14138e1c2d1545f5a6e346abbb9ffcb763b07
unknown <caymantomsic@gmail.com>
2021-02-04 16:35:57 -0800

    added details to bio, working on synth page

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('./config/mongoose.config'); // This is new
+app.use(cors());
+app.use(express.json()); // This is new
+app.use(express.urlencoded({ extended: true })); // This is new
+require('./routes/product.routes')(app);
+app.listen(8000, () => {
+    console.log("Listening at Port 8000")
+});
