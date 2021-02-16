1f9eee8e56c4398cc12c0080344fc4d2f64d1c98
CaymanTomsic <caymantomsic@gmail.com>
2021-02-15 19:53:57 -0800

    aligned experience page

--- client/src/components/ExperiencePage.js
+++ client/src/components/ExperiencePage.js
@@ -5,30 +5,43 @@ const ExperiencePage = (props) => {
     return (
         <div className="container my-4">
             <div className="row justify-content-center">
-                <h3 className= "whitesmoke">Experience:</h3>
-                <hr/>
-                <div className="row d-flex justify-content-center">
-                    <p className="whitesmoke w-50">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et. Ut wisi enim ad minim veniam, quis nostrud.
-                    </p>
-                </div>
-                <div className="row d-flex justify-content-center">
-                    <p className="whitesmoke w-50">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
-                        Ut wisi enim ad minim veniam, quis nostrud.
-                        Exerci tation ullamcorper suscipit <a href="#">lobortis nisl</a> ut aliquip ex ea commodo consequat.
-                        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl.
-                    </p>
-                </div>
-                <div className="row d-flex justify-content-center"></div>
-                    <p className= "whitesmoke w-50">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
-                        Ut wisi enim ad minim veniam, quis nostrud.
-                        Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-                    </p>
+                <div className="col section-2-box wow fadeInLeft">
+                    <h3 className= "whitesmoke">Experience:</h3>
+                    <hr/>
+                    <div className="justify-content-center" style={{textAlign: "left"}}>
+                        <div className="row align-items-center">
+                            <div className="d-flex justify-content-center">
+                                <p className="whitesmoke w-50 indent">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+                                    Ut wisi enim ad minim veniam, quis nostrud.
+                                    Exerci tation ullamcorper suscipit <a href="#">lobortis nisl</a> ut aliquip ex ea commodo consequat.
+                                    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row align-items-center">
+                            <div className="d-flex justify-content-center">
+                                <p className="whitesmoke w-50 indent">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+                                    Ut wisi enim ad minim veniam, quis nostrud.
+                                    Exerci tation ullamcorper suscipit <a href="#">lobortis nisl</a> ut aliquip ex ea commodo consequat.
+                                    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row align-items-center">
+                            <div className="d-flex justify-content-center">
+                                <p className= "whitesmoke w-50 indent">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+                                    Ut wisi enim ad minim veniam, quis nostrud.
+                                    Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
     );
 };
 
