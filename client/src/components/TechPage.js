45414b3b9080c621c213f845a368e45c973c7099
CaymanTomsic <caymantomsic@gmail.com>
2021-06-09 14:49:15 -0700

    tech formatting changes

--- client/src/components/TechPage.js
+++ client/src/components/TechPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TechPage = (props) => {
-// ADD PICTURE
+// maybe change to table/list
     return (
         <div className="container my-4">
             <div className="row justify-content-center">
@@ -9,32 +9,30 @@ const TechPage = (props) => {
                     <h3 className="whitesmoke">Technologies:</h3>
                     <hr />
                     <h6 className="bluetext" >Software Development:</h6>
-                    <div className="d-flex justify-self-center flex-column align-items-start" >
-                        <p className="medium-paragraph whitesmoke">
-                            <span className="pinktext"> Coding Languages</span>: C#, Python, JavaScript, CSS, HTML
-                        </p>
-                        <p className="medium-paragraph whitesmoke">
-                            <span className="pinktext">Frameworks/Libraries</span>: ASP.NET, Django, Express, React, Node, Bootstrap, jQuery
-                        </p>
-                        <p className="medium-paragraph whitesmoke">
-                            <span className="pinktext">Databases</span>: MongoDB, SQLite, MySQL
-                        </p>
-                        <p className="medium-paragraph whitesmoke">
-                            <span className="pinktext">Deployment Tools</span>: Amazon Web Services (AWS), Nginx, Gunicorn
-                        </p>
-                    </div>
+                    <p className="medium-paragraph whitesmoke text-left">
+                        <span className="pinktext"> Coding Languages</span>: C#, Python, JavaScript, CSS, HTML
+                    </p>
+                    <p className="medium-paragraph whitesmoke text-left">
+                        <span className="pinktext">Frameworks/Libraries</span>: ASP.NET, Django, Express, React, Node, Bootstrap, jQuery
+                    </p>
+                    <p className="medium-paragraph whitesmoke text-left">
+                        <span className="pinktext">Databases</span>: MongoDB, SQLite, MySQL
+                    </p>
+                    <p className="medium-paragraph whitesmoke text-left">
+                        <span className="pinktext">Deployment Tools</span>: Amazon Web Services (AWS), Nginx, Gunicorn
+                    </p>
                     <h6 className="bluetext" >Producing/Audio Engineering:</h6>
-                    <p className="medium-paragraph whitesmoke" style={{textAlign: "left"}}>
+                    <p className="medium-paragraph whitesmoke text-left" style={{textAlign: "left"}}>
                         <span className="pinktext">DAWs</span>: Pro Tools, Ableton Live, Cubase, and Logic
                     </p>
-                    <p className="medium-paragraph whitesmoke" style={{textAlign: "left"}}>
+                    <p className="medium-paragraph whitesmoke text-left" style={{textAlign: "left"}}>
                         <span className="pinktext">Plug-Ins</span>: Izotope RX, Antares AutoTune, UAD Plug-ins, NI Komplete Kontrol, Serum, Pro-Q, etc.
                     </p>
                     <h6 className="bluetext" >Musical Instruments:</h6>
-                    <p className="medium-paragraph whitesmoke" style={{textAlign: "left"}}>
+                    <p className="medium-paragraph whitesmoke text-left" style={{textAlign: "left"}}>
                         <span className="pinktext">Instruments</span>: Electric/Acoustic/Classical Guitar, Drum Kit, Hand Percussion, Electric Bass, Synthesizer, and Keyboard
                     </p>
-                    <p className="medium-paragraph whitesmoke" style={{textAlign: "left"}}>
+                    <p className="medium-paragraph whitesmoke text-left" style={{textAlign: "left"}}>
                         <span className="pinktext">Genres</span>: Jazz, Pop, RnB, Rock, Blues, Indie, Metal, Classical, Prog, Electronic, etc.
                     </p>
                 </div>
